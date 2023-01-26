b3ddfd34237ffb2e454edbfb762822ccb3c00db1
adeutou <a.deutou@gmail.com>
2023-01-26 19:57:49 +0100

    data import

---
+++ resources/views/users.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Import Export Excel & CSV File to Database</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="card mt-3 mb-3">
+            <div class="card-header text-center">
+                <h4>Laravel Import/Export Excel & CSV file to Database</h4>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('users.import') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <input type="file" name="file" class="form-control">
+                    <br>
+                    <button class="btn btn-primary">Import User Data</button>
+                </form>
+
+                <table class="table table-bordered mt-3">
+                    <tr>
+                        <th colspan="3">
+                            List Of Users
+                            <a class="btn btn-danger float-end" href="{{ route('users.export') }}">Export User Data</a>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                    @foreach($users as $user)
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                    </tr>
+                    @endforeach
+                </table>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
